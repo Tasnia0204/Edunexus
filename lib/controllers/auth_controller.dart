7fcde8bf3fc1c492b74e977546e27dc6f45683c3
tasnia <tasniajahan0204@gmail.com>
2025-08-15 11:08:28 +0600

    Delete lib directory

--- lib/controllers/auth_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AuthController {
-  void showError(String message, BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
-  }
-}
